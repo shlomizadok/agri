bca715a5dc6a09f0cd8e85a10386fd9ad68777bd
Shlomi Zadok <shlomi@ben-hanna.com>
2016-05-15 13:08:45 +0300

    Agri (Haklaim) ionic initial commit

---
+++ www/js/app.js
@@ -0,0 +1,127 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngResource', 'ngMessages'])
+
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
+
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
+  })
+
+  .config(function ($stateProvider, $urlRouterProvider, $httpProvider, $ionicConfigProvider) {
+    $stateProvider
+
+      .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
+      })
+
+      .state('app.login', {
+        url: '/login',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/login.html',
+            controller: 'LoginCtrl'
+          }
+        }
+      })
+
+      .state('app.signup', {
+        url: '/signup',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/signup.html',
+            controller: 'SignupCtrl'
+          }
+        }
+      })
+
+      .state('app.search', {
+        url: '/search',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/search.html'
+          }
+        }
+      })
+
+      .state('app.profile', {
+        url: '/profile',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/user.html',
+            controller: 'ProfileCtrl'
+          }
+        }
+      })
+
+      .state('app.profileEdit', {
+        url: '/profile-edit',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/user-edit.html',
+            controller: 'ProfileEditCtrl'
+          }
+        }
+      })
+
+      .state('app.sales', {
+        url: '/sales',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/sales.html',
+            controller: 'SalesCtrl'
+          }
+        }
+      })
+
+      .state('app.futureSales', {
+        url: '/future-sales',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/sales.html',
+            controller: 'FutureSalesCtrl'
+          }
+        }
+      })
+
+      .state('app.newSale', {
+        url: '/new-sale',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/new-sale.html',
+            controller: 'newSaleCtrl'
+          }
+        }
+      })
+
+      .state('app.single', {
+        url: '/sales/:saleId',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/sale.html',
+            controller: 'SaleCtrl'
+          }
+        }
+      });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/sales');
+    $httpProvider.interceptors.push('AuthInterceptor');
+    $ionicConfigProvider.views.maxCache(0);
+  });
---
+++ www/js/controllers.js
@@ -0,0 +1,146 @@
+angular.module('starter.controllers', [])
+  .controller('AppCtrl', function ($http, $scope, $log, User) {
+
+
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+
+    $scope.currentUser = User.loggedIn();
+  })
+
+  .controller('LoginCtrl', function ($scope, $http, $log, $state, User) {
+    if (User.loggedIn() == true) return $state.go('app.profile');
+    $scope.loginData = {};
+
+    $scope.doLogin = function () {
+      $http.post('https://agri-il.herokuapp.com/v1/login', {
+        email: $scope.loginData.username,
+        password: $scope.loginData.password
+      }).then(function (response) {
+        window.localStorage['authToken'] = response.data.access_token;
+        window.localStorage['userId'] = response.data.id;
+        // @TODO: persist user somehow
+        User.setUser(response.data);
+        $scope.currentUser = true;
+        return $state.go('app.profile');
+      }, function (error) {
+        alert('Incorrect password - please try again.')
+        $log.log(error);
+      });
+    };
+  })
+
+  .controller('SignupCtrl', function ($scope, $http, $log, $state, User) {
+    if (User.loggedIn() == true) return $state.go('app.profile');
+    $scope.signupData = {};
+    $scope.doSignup = function () {
+      $http.post('https://agri-il.herokuapp.com/v1/user', {
+        user: {
+          email: $scope.signupData.email,
+          password: $scope.signupData.password,
+          password_confirmation: $scope.signupData.password_confirmation
+        }
+      }).then(function (response) {
+        window.localStorage['authToken'] = response.data.access_token;
+        window.localStorage['userId'] = response.data.id;
+        // @TODO: persist user somehow
+        User.setUser(response.data);
+        return $state.go('app.profile');
+      }, function (error) {
+        alert('Incorrect password - please try again.')
+        $log.log(error);
+      });
+    };
+  })
+
+  .controller('SalesCtrl', function ($scope, Sales, Regions) {
+    $scope.regions = Regions.query();
+    $scope.sales = Sales.query();
+    $scope.futureButton = true;
+  })
+
+  .controller('FutureSalesCtrl', function ($scope, Sales, Regions) {
+    $scope.regions = Regions.query();
+    $scope.sales = Sales.query({future: true})
+  })
+
+  .controller('SaleCtrl', function ($scope, $stateParams, $state, $http, Sales, User) {
+    $scope.sale = Sales.get({id: $stateParams.saleId});
+    if (User.loggedIn() === true) {
+      $http.post('https://agri-il.herokuapp.com/v1/me/can', {
+        sale_id: $stateParams.saleId
+      }).then(function (response) {
+        $scope.canEdit = response.data;
+      }, function (error) {
+        // Errored, defaulting to can't edit.
+        $scope.canEdit = false;
+      })
+      $scope.updateSale = function () {
+        $scope.sale.$update(function () {
+
+        })
+      }
+      $scope.deleteSale = function () {
+        $scope.sale.$delete(function () {
+          $state.go('app.profile');
+        })
+      }
+    }
+  })
+
+  .controller('ProfileEditCtrl', function ($scope, $stateParams, $state, User, Regions, Cities, Profile) {
+    if (User.loggedIn() == false) return $state.go('app.login');
+    $scope.profile = new Profile()
+    $scope.regions = Regions.query();
+    $scope.settlements = Cities.list();
+
+    $scope.submitProfile = function (form) {
+      console.log($scope.profile)
+      if (form.$valid) {
+        $scope.profile.$save(function () {
+          $state.go('app.profile');
+        })
+      } else {
+        alert('Missing data....')
+      }
+    }
+  })
+
+  .controller('ProfileCtrl', function ($scope, $stateParams, $state, User, Sales) {
+    if (User.loggedIn() == false) return $state.go('app.login');
+    User.me().success(function (response) {
+      $scope.user = response;
+      $scope.profile = $scope.user.profile;
+      if ($scope.profile == null) {
+        return $state.go('app.profileEdit');
+      }
+    });
+    $scope.sales = Sales.query({id: 'me'});
+    $scope.newSale = function () {
+      return $state.go('app.newSale')
+    }
+  })
+
+  .controller('newSaleCtrl', function ($scope, $stateParams, $state, $http, User, Sales) {
+    if (User.loggedIn() == false) return $state.go('app.login');
+    $http.get('https://agri-il.herokuapp.com/v1/vegs').success(function (response) {
+      $scope.vegs = response;
+    });
+    $scope.selected_vegs = [];
+    $scope.toggleSelection = function toggleSelection(fruitName) {
+      var idx = $scope.selected_vegs.indexOf(fruitName);
+      if (idx > -1) {
+        $scope.selected_vegs.splice(idx, 1);
+      } else {
+        $scope.selected_vegs.push(fruitName);
+      }
+    };
+    $scope.sale = new Sales();
+    $scope.saveSale = function () {
+      $scope.sale.veg_list = $scope.selected_vegs;
+      if ($scope.sale.new_veg) $scope.sale.veg_list.push($scope.sale.new_veg)
+      $scope.sale.$save(function () {
+        $state.go('app.profile');
+      })
+    }
+  })
